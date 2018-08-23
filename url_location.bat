8c613170606fa0e766217516885753e044d815ce
abeanari <peter4bbit@gmail.com>
2018-08-22 23:05:46 -0700

    Update url_location.bat
    Add menu option, loops until exit option chosen

--- url_location.bat
+++ url_location.bat
@@ -1,4 +1,16 @@
 @echo off
-set /P input= Type in ip: 
+CLS
+
+:MENU
+::CLS ::uncomment if you want to clear results
+set /P input= Type ip address or press q to EXIT: 
+if %input%==q goto QUIT
 curl ipinfo.io/%input%
-pause
+echo:
+echo:
+goto MENU
+
+:QUIT
+echo Goodbye!
+PAUSE>NUL
+EXIT
